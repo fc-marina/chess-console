2edc0018077b1f2834a97e63a506e6bd5936fbe3
Marina Faria <fc.marina@outlook.com>
2023-04-28 22:48:43 -0300

    Criação classes: Board, Color e Piece

---
+++ chess-console/board/Board.cs
@@ -0,0 +1,17 @@
+﻿namespace board
+{
+    class Board
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        private Piece[,] _pieces;
+
+        public Board(int rows, int columns)
+        {
+            Columns = columns;
+            Rows = rows;
+            _pieces = new Piece[rows, columns];
+        }
+    }
+}
