08069e75271630ff6fc10318461c996e1c2edfe7
Marina Faria <fc.marina@outlook.com>
2023-05-09 01:58:08 -0300

    Tratando erros com excecao personalizada

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -2,9 +2,16 @@
 using chess;
 using chess_console;
 
-Board board = new Board(8,8);
-board.PutPiece(new Rook(board, Color.Black), new Position(0, 0));
-board.PutPiece(new Rook(board, Color.Black), new Position(1, 3));
-board.PutPiece(new King(board, Color.Black), new Position(2, 4));
-Screen.PrintBoard(board);
+try
+{
+    Board board = new Board(8, 8);
+    board.PutPiece(new Rook(board, Color.Black), new Position(0, 0));
+    board.PutPiece(new Rook(board, Color.Black), new Position(1, 3));
+    board.PutPiece(new King(board, Color.Black), new Position(2, 3));
+    Screen.PrintBoard(board);
+}
+catch (BoardException ex)
+{
+    Console.WriteLine(ex.Message);
+}
 Console.ReadLine();
--- chess-console/board/Board.cs
+++ chess-console/board/Board.cs
@@ -19,10 +19,42 @@
             return _pieces[row, column];
         }
 
+        public Piece Piece(Position position)
+        {
+            return _pieces[position.Row, position.Column];
+        }
+
         public void PutPiece(Piece piece, Position position)
         {
+            if (DoesPieceExists(position))
+            {
+                throw new BoardException("There is already a piece at that position!");
+            }
             _pieces[position.Row, position.Column] = piece;
             piece.Position = position;
+        }
+
+        public bool IsPositionValid(Position position)
+        {
+            if (position.Row < 0 || position.Row >= Rows || position.Column < 0 || position.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DoesPieceExists(Position position)
+        {
+            ValidatePosition(position);
+            return Piece(position) != null;
+        }
+
+        public void ValidatePosition(Position position)
+        {
+            if (!IsPositionValid(position))
+            {
+                throw new BoardException("Invalid Position!");
+            }
         }
     }
 }
