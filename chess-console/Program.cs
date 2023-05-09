6eac6361f2fd45904c1df432074deaf728e2ab9f
Marina Faria <fc.marina@outlook.com>
2023-05-09 02:32:46 -0300

    Melhorando a implementação do método imprimir tabuleiro

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -2,19 +2,18 @@
 using chess;
 using chess_console;
 
-//try
-//{
-//    Board board = new Board(8, 8);
-//    board.PutPiece(new Rook(board, Color.Black), new Position(0, 0));
-//    board.PutPiece(new Rook(board, Color.Black), new Position(1, 3));
-//    board.PutPiece(new King(board, Color.Black), new Position(2, 3));
-//    Screen.PrintBoard(board);
-//}
-//catch (BoardException ex)
-//{
-//    Console.WriteLine(ex.Message);
-//}
-ChessPosition position = new ChessPosition('c', 7);
-Console.WriteLine(position);
-Console.WriteLine(position.ToPosition());
+try
+{
+    Board board = new Board(8, 8);
+    board.PutPiece(new Rook(board, Color.White), new Position(0, 0));
+    board.PutPiece(new Knight(board, Color.Black), new Position(1, 3));
+    board.PutPiece(new King(board, Color.White), new Position(1, 4));
+    board.PutPiece(new Queen(board, Color.Black), new Position(3, 2));
+    Screen.PrintBoard(board);
+}
+catch (BoardException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
 Console.ReadLine();
--- chess-console/Screen.cs
+++ chess-console/Screen.cs
@@ -8,6 +8,7 @@ namespace chess_console
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.Piece(i, j) == null)
@@ -16,11 +17,31 @@ namespace chess_console
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
 
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+
             }
         }
     }
